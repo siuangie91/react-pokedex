bcc37cec9773bb214032b01f99f5c2e4bcbc58d5
siuangie91 <siuangie91@gmail.com>
2019-05-08 23:53:56 -0400

    init commit; create dummy data

---
+++ src/data/pokemon.js
@@ -0,0 +1,46 @@
+// image paths are https://www.serebii.net/pokearth/sprites/crystal/152.png
+
+const pokemon = [
+  {
+    id: 152,
+    name: "Chikorita",
+  },
+  {
+    id: 155,
+    name: "Cyndaquil",
+  },
+  {
+    id: 158,
+    name: "Totodile",
+  },
+  {
+    id: 173,
+    name: "Clefa",
+  },
+  {
+    id: 174,
+    name: "Igglybuff",
+  },
+  {
+    id: 175,
+    name: "Togepi",
+  },
+  {
+    id: 182,
+    name: "Bellossom",
+  },  
+  {
+    id: 202,
+    name: "Wobbuffet",
+  },
+  {
+    id: 220,
+    name: "Swinub",
+  },
+  {
+    id: 246,
+    name: "Larvitar",
+  },
+];
+
+export default pokemon;
