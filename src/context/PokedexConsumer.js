c28c8b9ff5bdc092730ef0f2d631c1b293d80b46
Siu <210014@Ansi8039-MKT.home>
2019-05-10 13:44:42 -0400

    remove unnecessary props spreading

--- src/context/PokedexConsumer.js
+++ src/context/PokedexConsumer.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import PokedexContext from './PokedexContext';
 
 /*
-1a. Create a higher order component, which essentially takes a wrapped component and
-returns a new component with the wrappe component inside it. 
+1. Create a higher order component, which essentially takes a wrapped component and
+returns a new component with the wrapped component inside it. 
 This allows the wrapped component to access the props provided by the wrapper.
 */
 const withPokedexContext = WrappedComponent => {
@@ -11,16 +11,16 @@ const withPokedexContext = WrappedComponent => {
     render() {
       return (
         /*
-        1b. For our HOC, we want any component wrapped by withPokedexContext to have access to the Context.
-        So we wrap the WrappedComponent that's passed in inside a Consumer.
+        1a. For our HOC, we want any component wrapped by withPokedexContext to have access to the Context.
+        So we wrap the WrappedComponent inside a Consumer.
         */
         <PokedexContext.Consumer>
           {
             /*
-            1c. Previously, we passed in a function of this sort inside `DropdownSection` and `PokedexSection`.
+            1b. Previously, we passed in a function of this sort inside `DropdownSection` and `PokedexSection`.
             Now we can just do it here, and pass the value to the WrappedComponent.
             */
-            value => <WrappedComponent pokedexContext={value} {...this.props} />
+            value => <WrappedComponent pokedexContext={value} />
           }
         </PokedexContext.Consumer>     
       )
